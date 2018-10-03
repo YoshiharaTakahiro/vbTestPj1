cfc28bed75859f261337eface7e7b42aebd91949
良原 貴弘 <tyoshihara@ecc.ac.jp>
2018-10-03 20:01:36 +0900

    Revert "Revert "Add TextBox1""
    This reverts commit 1050fdf57539b1a8d93fe67646cce984ade13ec7.

--- GitTest1/GitTest1/Form1.vb
+++ GitTest1/GitTest1/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Label1.Text = "Hello World"
     End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Label2.Text = TextBox1.Text
+    End Sub
 End Class
